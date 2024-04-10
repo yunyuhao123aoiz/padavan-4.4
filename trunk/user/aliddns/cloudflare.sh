686616fc9287e6a30fcaf7ffda355ae808874d1e
yunyuhao123aoiz <125276687+yunyuhao123aoiz@users.noreply.github.com>
2024-04-10 13:25:12 +0800

    Update and rename Sh44_cloudflare.sh to cloudflare.sh

--- trunk/user/aliddns/cloudflare.sh
+++ trunk/user/aliddns/cloudflare.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 #copyright by hiboy
-source /etc/storage/script/init.sh
-cloudflare_enable=`nvram get cloudflare_enable`
-[ -z $cloudflare_enable ] && cloudflare_enable=0 && nvram set cloudflare_enable=0
-if [ "$cloudflare_enable" != "0" ] ; then
-#nvramshow=`nvram showall | grep '=' | grep cloudflare | awk '{print gensub(/'"'"'/,"'"'"'\"'"'"'\"'"'"'","g",$0);}'| awk '{print gensub(/=/,"='\''",1,$0)"'\'';";}'` && eval $nvramshow
+#source /etc/storage/init.sh
+#ACTION=$1
+export PATH='/etc/storage/bin:/tmp/script:/etc/storage/script:/opt/usr/sbin:/opt/usr/bin:/opt/sbin:/opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'
+export LD_LIBRARY_PATH=/lib:/opt/lib
+ACTION=$1
+scriptfilepath=$(cd "$(dirname "$0")"; pwd)/$(basename $0)
+#echo $scriptfilepath
+scriptpath=$(cd "$(dirname "$0")"; pwd)
+#echo $scriptpath
+scriptname=$(basename $0)
+
+aliddns_enable=`nvram get aliddns_enable`
+[ -z $aliddns_enable ] && aliddns_enable=0 && nvram set aliddns_enable=0
+if [ "$aliddns_enable" != "0" ] ; then
+#nvramshow=`nvram showall | grep '=' | grep aliddns | awk '{print gensub(/'"'"'/,"'"'"'\"'"'"'\"'"'"'","g",$0);}'| awk '{print gensub(/=/,"='\''",1,$0)"'\'';";}'` && eval $nvramshow
 
 cloudflare_token=`nvram get cloudflare_token`
 cloudflare_Email=`nvram get cloudflare_Email`
